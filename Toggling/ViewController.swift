19d156cea89bd79a816b5fcf4781a0a11fdd194f
Trevor Beasty <trevor.beasty@weightwatchers.com>
2019-03-06 14:05:24 -0500

    Initial Commit

---
+++ Toggling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toggling
+//
+//  Created by Trevor Beasty on 3/6/19.
+//  Copyright © 2019 Trevor Beasty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
