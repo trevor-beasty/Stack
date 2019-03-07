46e87e19c15e531e9bc70ff36b1e3487a35fd18c
Trevor Beasty <trevor.beasty@weightwatchers.com>
2019-03-07 09:10:03 -0500

    compound toggle

--- Toggling/ViewController.swift
+++ Toggling/ViewController.swift
@@ -16,9 +16,9 @@ class FlatToggle: UIViewController {
     
     let tap = UITapGestureRecognizer()
     
-    let c0 = FlatToggle.createComponent(color: .red, text: "21")
-    let c1 = FlatToggle.createComponent(color: .blue, text: "Savage")
-    let c2 = FlatToggle.createComponent(color: .green, text: "21, 21, 21")
+    let c0 = UIView.createComponent(color: .red, text: "21")
+    let c1 = UIView.createComponent(color: .blue, text: "Savage")
+    let c2 = UIView.createComponent(color: .green, text: "21, 21, 21")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,17 +57,84 @@ class FlatToggle: UIViewController {
         }
     }
     
-    static func createComponent(color: UIColor, text: String) -> UIView {
-        let component = UIView()
-        let label = UILabel()
-        label.text = text
-        label.centerIn(component)
-        label.textColor = .white
-        component.translatesAutoresizingMaskIntoConstraints = false
-        component.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        component.backgroundColor = color
+}
+
+class CompoundToggle: UIViewController {
+    
+    let scroll = UIScrollView()
+    let container = UIView()
+    let stack = UIStackView()
+    
+    let tap = UITapGestureRecognizer()
+    
+    let c0 = CompoundToggleComponent()
+    let c1 = CompoundToggleComponent()
+    let c2 = CompoundToggleComponent()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        return component
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.spacing = 0
+        
+        scroll.fill(view)
+        
+        container.fill(scroll)
+        container.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        
+        stack.fill(container)
+        
+        [c0, c1, c2].forEach({
+            stack.addArrangedSubview($0)
+        })
+        
+        view.backgroundColor = .white
+        
+    }
+    
+}
+
+class CompoundToggleComponent: UIView {
+    
+    let stack = UIStackView()
+    let c0 = UIView.createComponent(color: .purple, text: "Tier X")
+    let c1 = UIView.createComponent(color: .yellow, text: "Grapes all day!!")
+    
+    let tap = UITapGestureRecognizer()
+    
+    init() {
+        super.init(frame: .zero)
+        
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.spacing = 0
+        
+        stack.fill(self)
+        
+        [c0, c1].forEach({
+            stack.addArrangedSubview($0)
+        })
+        
+        tap.addTarget(self, action: #selector(didTap))
+        c0.addGestureRecognizer(tap)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) { fatalError() }
+    
+    @objc func didTap() {
+        toggleC1()
+    }
+    
+    func toggleC1() {
+        c1.alpha = c1.isHidden ? 1 : 0
+        UIView.animate(withDuration: 1.0) {
+            self.c1.isHidden = !self.c1.isHidden
+            self.stack.layoutIfNeeded()
+        }
     }
     
 }
@@ -94,6 +161,19 @@ extension UIView {
             centerYAnchor.constraint(equalTo: other.centerYAnchor)
         ]
         NSLayoutConstraint.activate(constraints)
+    }
+    
+    static func createComponent(color: UIColor, text: String) -> UIView {
+        let component = UIView()
+        let label = UILabel()
+        label.text = text
+        label.centerIn(component)
+        label.textColor = .white
+        component.translatesAutoresizingMaskIntoConstraints = false
+        component.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        component.backgroundColor = color
+        
+        return component
     }
     
 }
